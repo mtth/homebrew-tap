1519a12f3cb66235c2249129b8a17506567ce183
Matthieu Monsch <matthieu.monsch@gmail.com>
2025-07-30 08:54:35 -0700

    chore(git-jot): add SHA

--- Formula/git-jot.rb
+++ Formula/git-jot.rb
@@ -4,11 +4,12 @@ class GitJot < Formula
   license "MIT"
   version "0.0.5"
   url "https://github.com/mtth/git-jot/archive/refs/tags/v0.0.5.tar.gz"
-  # sha256 "TODO"
+  sha256 "ff6390c3cf4598d24ccf190064f0d2bc04ff955e651cefccd2ceab549900285d"
 
   depends_on "git"
 
   def install
+    # TODO: Build and install manpage.
     bin.install "git-jot.sh" => "git-jot"
   end
 end
