9d68e22799a862d605b2d00b249cfb7fc8275388
Matthieu Monsch <matthieu.monsch@gmail.com>
2026-03-20 14:26:59 -0700

    chore(git-jot): bump version

--- Formula/git-jot.rb
+++ Formula/git-jot.rb
@@ -2,9 +2,9 @@ class GitJot < Formula
   desc "Git commands to emulate branch notes"
   homepage "https://github.com/mtth/git-jot"
   license "MIT"
-  version "0.0.5"
-  url "https://github.com/mtth/git-jot/archive/refs/tags/v0.0.5.tar.gz"
-  sha256 "ff6390c3cf4598d24ccf190064f0d2bc04ff955e651cefccd2ceab549900285d"
+  version "0.4.1"
+  url "https://github.com/mtth/git-jot/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "9916369b9c7d541174a1160ebb0c41813eab664ccdb41087dfb421b0dfa23c9f"
 
   depends_on "git"
 
