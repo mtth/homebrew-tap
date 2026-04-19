9938b607dd9daa1e72a4871b966ea6728a7d7e6a
Matthieu Monsch <matthieu.monsch@gmail.com>
2026-04-18 19:18:26 -0700

    feat(git-jot): install manpage

--- Formula/git-jot.rb
+++ Formula/git-jot.rb
@@ -6,10 +6,17 @@ class GitJot < Formula
   url "https://github.com/mtth/git-jot/archive/refs/tags/v0.7.0.tar.gz"
   sha256 "b1e657221e561635a0df02ebd9f605698cc2b36976546f77dc9b62dec3a8a4e9"
 
+  depends_on "asciidoctor" => :build
   depends_on "git"
 
   def install
-    # TODO: Build and install manpage.
+    system "asciidoctor", "-b", "manpage",
+           "-a", "mansource=git-jot v#{version}",
+           "-a", "manmanual=git-jot manual",
+           "-o", "git-jot.1", "git-jot.1.adoc"
+    system "gzip", "-9", "git-jot.1"
+
+    man1.install "git-jot.1.gz"
     bin.install "git-jot.sh" => "git-jot"
   end
 end
