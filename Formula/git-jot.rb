7d4ad47b7fbd90a7c3dc4a6f36c0c30f73fe6d6b
Matthieu Monsch <matthieu.monsch@gmail.com>
2026-04-18 19:32:05 -0700

    feat(git-jot): support building from HEAD

--- Formula/git-jot.rb
+++ Formula/git-jot.rb
@@ -5,13 +5,16 @@ class GitJot < Formula
   version "0.7.0"
   url "https://github.com/mtth/git-jot/archive/refs/tags/v0.7.0.tar.gz"
   sha256 "b1e657221e561635a0df02ebd9f605698cc2b36976546f77dc9b62dec3a8a4e9"
+  head "https://github.com/mtth/git-jot.git", branch: "main"
 
   depends_on "asciidoctor" => :build
   depends_on "git"
 
   def install
+    source_version = build.head? ? "HEAD" : "v#{version}"
+
     system "asciidoctor", "-b", "manpage",
-           "-a", "mansource=git-jot v#{version}",
+           "-a", "mansource=git-jot #{source_version}",
            "-a", "manmanual=git-jot manual",
            "-o", "git-jot.1", "git-jot.1.adoc"
     system "gzip", "-9", "git-jot.1"
